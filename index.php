ccfad316e15fbf80e5dec7b83d952b600fe12030
jheg <emailheg@gmail.com>
2013-11-18 23:50:05 +0000

    2nd commit

---
+++ index.php
@@ -0,0 +1,136 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/grid.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/nav.css">
+<link rel="stylesheet" type="text/css" href="css/supersized.core.css"  media="screen" />
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js"></script>
+<script  src="js/supersized.core.3.2.1.min.js"></script>
+<script type="text/javascript" src="js/masonry.pkgd.min.js"></script>
+<script type="text/javascript">
+  
+  jQuery(function($){
+    
+    $.supersized({
+      slides  :   [ {image : 'img/bw.jpg', title : 'Image Credit: '} ]
+    });
+    });
+    
+</script>
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0">
+
+</head>
+<body>
+  <div class="">
+    <nav id="nav" role="navigation">
+    <a href="#nav" title="Show navigation">Show navigation</a>
+    <a href="#" title="Hide navigation">Hide navigation</a>
+      <ul>
+        <li><a href="">Nav1</a></li>
+        <li><a href="">Nav2</a></li>
+        <li><a href="">Nav3</a></li>
+        <li><a href="">Nav4</a></li>
+        <li><a href="">Nav5</a></li>
+        <li><a href="">Nav6</a></li>
+        <li><a href="">Nav7</a></li>
+        <li><a href="">Nav8</a></li>
+        <li><a href="">Nav9</a></li>
+        <li><a href="">Nav10</a></li>
+      </ul>
+    </nav>
+  </div>
+<div  class="container main">
+
+  <div class="grid_12 clearfix">
+
+
+    <div class="contact right"><h2>Get in touch</h2><p class="tel">01473 221399</p></div>
+  </div>
+  <div id="container" class="grid_12 clearfix js-masonry" data-masonry-options='{ "columnWidth": 300, "itemSelector": ".item" }'>  
+    <!-- Scrolling featured properties -->
+    <div class="property item">
+      <h2>Featured property</h2>
+      <div>
+        <img src="img/no_image.jpg">
+        <ul>
+          <li>3 Bed Semi det in good order</li>
+          <li>Ipswich</li>
+          <li>&pound;135,000</li>
+        </ul>
+      </div>
+    </div>
+    <div class="social item">
+      <h2>Latest post</h2>
+
+      <div>
+        <h3>Preparing your home for sale</h3>
+        <p>It is important that you present your property for sale in the best possible...</p>
+      </div>
+    </div>
+    <div class="info item">
+      <h2>Estate agent Ipswich</h2>
+      <div>
+        <p>Since we opened our doors in 2010 we have gone from strength to strength whilst not losing sight of what is important to our customers.</p>
+      </div>
+    </div>
+    <div class="social item">
+      <h2>Follow Us</h2>
+      <div>
+        <ul>
+          <li>Facebook</li>
+          <li>Twitter</li>
+          <li>LinkedIn</li>
+        </ul>
+      </div>
+    </div>
+    <div class="social item">
+      <h2>Featured property</h2>
+      <div>
+        <img src="img/no_image.jpg">
+        <ul>
+          <li>3 Bed Semi</li>
+          <li>Ipswich</li>
+          <li>&pound;135,000</li>
+        </ul>
+      </div>
+    </div>
+    <div class="info item">
+      <h2>December Special Offer</h2>
+      <div>
+        <p>We are offering a fantastic rate of 1% for any instructions taken in the month of December.</p>
+      </div>
+    </div>
+    <div class="social item">
+      <h2>Testimonials</h2>
+      <div>
+        <h3>Mr &amp; Mrs Smith</h3>
+        <p>Excellent service start to finish - well done to the whole team!</p>
+      </div>
+    </div>
+    <div class="contact item">
+      <h2>Our address</h2>
+      <div>
+        <address><p>
+          St Peters Street<br />
+          Ipswich<br />
+          Suffolk<br />
+          IP1 1PD</p>
+        </address>
+      </div>
+    </div>
+    </div>
+    <div class=""></div>
+    <div class=""></div>
+    <div class=""></div>
+    <div class=""></div>
+    <div class=""></div>
+    <div class=""></div>
+    <div class=""></div>
+    <div class=""></div>
+
+  
+
+</div>
+</body>
+</html>
